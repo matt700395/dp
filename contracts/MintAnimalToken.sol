afa14ef9bcca31d706fc664c14894fa0a5b1a4b0
matt700395 <matt7003@korea.ac.kr>
2022-06-15 14:12:47 +0900

    complete sol

--- contracts/MintAnimalToken.sol
+++ contracts/MintAnimalToken.sol
@@ -7,13 +7,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MintAnimalToken is ERC721Enumerable {
     constructor() ERC721("h662Animals", "HAS") {}
 
+    mapping(uint256 => uint256) public animalTypes;
+
     function mintAnimalToken() public {
-        uint256 animalTokenId = totalSupply() + 1; // tokenid를 매번 1씩 올림
+        uint256 animalTokenId = totalSupply() + 1;
 
         uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1;
 
         animalTypes[animalTokenId] = animalType;
 
-        _mint(msg.sender, animalTokenId); // erc721에서 제공해주는 민팅하는 함수임 
+        _mint(msg.sender, animalTokenId);
     }
 } 
