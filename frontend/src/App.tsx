2c180ce18e5804a2cfd8a909f295b30e5ae9ac19
matt700395 <matt7003@korea.ac.kr>
2022-06-15 21:18:16 +0900

    setting metamask login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,30 @@ import Main from "./routes/main";
 import { mintAnimalTokenContract } from "../contracts/index";
 
 const App: FC = () => {
+//메타 마스크 연결하기
+  const [account, setAccount] = useState<string>("");
+
+  const getAccount = async ()=>{
+    try {
+      if(window.ethereum){ // 메타마스크가 설치되어있으면 로그인 실행
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        setAccount(accounts[0]);
+      }else{
+        alert("please install Metamask!")
+      }
+    }catch (error){
+
+    }
+  }
+
+  useEffect(() =>{
+    getAccount();
+  }, [account]);
+
+
   return <BrowserRouter>
   <Routes>
       <Route path="/" element={<Main />} />
--- frontend/src/routes/main.tsx
+++ frontend/src/routes/main.tsx
@@ -1,8 +1,11 @@
 import React, {FC} from 'react';
 import {Box} from "@chakra-ui/react";
+import App from "../App";
 
 const Main: FC = () => {
-    return <Box>Main</Box>
+
+
+    return <Box> Account :</Box>
 };
 
 export default Main;
