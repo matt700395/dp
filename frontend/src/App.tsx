86fb8b7073460f991a53d888580fccc1f3d8a405
matt700395 <matt7003@korea.ac.kr>
2022-06-15 20:42:52 +0900

    react ts chakra , dom setting 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { Button } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./routes/main";
 
 const App: FC = () => {
-  return <div>web3-boilerplate</div>;
+  return <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes></BrowserRouter>
 };
 
 export default App;
