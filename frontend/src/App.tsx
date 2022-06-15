328c5ccf8a775d0e7865ef3efa5cca0d64ff2067
matt700395 <matt7003@korea.ac.kr>
2022-06-15 20:58:59 +0900

    web3 setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect ,useState } from "react";
 import { Button } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/main";
+
+import { mintAnimalTokenContract } from "../contracts/index";
 
 const App: FC = () => {
   return <BrowserRouter>
